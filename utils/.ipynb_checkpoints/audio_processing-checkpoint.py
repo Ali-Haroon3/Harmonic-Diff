0297424153ce4976bd3652ff690fd9a954d3bff4
Ali Haroon <112351004+Ali-Haroon3@users.noreply.github.com>
2024-07-26 21:14:30 -0600

    Most of app working

--- utils/.ipynb_checkpoints/audio_processing-checkpoint.py
+++
@@ -1,8 +0,0 @@
-import librosa
-import numpy as np
-
-def process_audio(file_path):
-    y, sr = librosa.load(file_path)
-    # Compute the derivative of the waveform
-    derivative = np.diff(y)
-    return derivative, sr
--- utils/.ipynb_checkpoints/derivative-checkpoint.py
+++
@@ -1,4 +0,0 @@
-import soundfile as sf
-
-def calculate_derivative(derivative, sr, output_path):
-    sf.write(output_path, derivative, sr)
